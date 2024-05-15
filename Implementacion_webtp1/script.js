69c2d84abc8cd96135dd56addb7ad448d8fabbd8
KimeyLu <128751189+KimeyLu@users.noreply.github.com>
2024-05-15 19:58:53 -0300

    v1

---
+++ Implementacion_webtp1/script.js
@@ -0,0 +1,9 @@
+/*window.onload = function() {
+    var body = document.getElementsByTagName("body")[0];
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    body.style.width = width + "px";
+    body.style.height = height + "px";
+};*/
+
